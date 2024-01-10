536ce54f572003a6e1cffbd5838e5d7ce8754bfa
Joseph Diza <joseph@wizy.io>
2024-01-10 14:02:41 +0800

    setup prisma. removed server file in src

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,9 +5,15 @@ import cors from 'cors'
 // App variables
 dotenv.config()
 
+const PORT = process.env.PORT || 5001
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.listen(PORT, async () => {
+  console.log(`Listening on port:${PORT}`)
+})
 
 module.exports = app
--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const app = require('.')
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, async () => {
-  console.log(`Listening on port:${PORT}`)
-})
