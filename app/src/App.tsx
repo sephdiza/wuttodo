84cfc289586cee7a076ee39c3fdde100146514b6
Joseph Diza <joseph@wizy.io>
2024-01-10 17:05:31 +0800

    testing connection between server and client

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,41 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+
+interface Todo {
+  id: string,
+  name: string,
+  status: string,
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>()
+  const [error, setError] = useState('')
+
+  async function fetchTodos() {
+    try {
+      const response = await fetch('https://wuttodo.onrender.com/todos')
+      const data: Todo[] = await response.json()
+      setTodos(data)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch(error: any) {
+      console.error(error)
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  if (error) <div>Something went wrong</div>
+
   return (
-    <>
-      <div>Hello world!</div>
-    </>
+    <div>
+      {todos?.map((todo) => (
+        <li key={todo.id}>
+          {todo.name}: {todo.status}
+        </li>
+      ))}
+    </div>
   )
 }
 
