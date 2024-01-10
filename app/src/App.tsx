3826e7af415334caee9ac027f95f6faf372ebae4
Joseph Diza <joseph@wizy.io>
2024-01-11 00:38:30 +0800

    add process env api url

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,7 @@ interface Todo {
   status: string;
 }
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL;
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
