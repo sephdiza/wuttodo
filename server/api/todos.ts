0b372a3c774aa96e6a65ac8e898aa36b8ed0754d
Joseph Diza <joseph@wizy.io>
2024-01-13 01:28:00 +0800

    add sorting of todos by id. refactor, added semicolons

--- server/api/todos.ts
+++ server/api/todos.ts
@@ -1,45 +1,48 @@
-import express, { Request, Response, ErrorRequestHandler } from 'express'
-import prisma from '../src/prisma'
+import express, { Request, Response } from 'express';
+import prisma from '../src/prisma';
 
-
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const todos = await prisma.todoList.findMany()
-  res.send(todos)
-})
+	const todos = await prisma.todoList.findMany({
+		orderBy: {
+			id: 'asc',
+		},
+	});
+	res.send(todos);
+});
 
 router.post('/', async (req: Request, res: Response) => {
-  try {
-    const { name } = req.body
+	try {
+		const { name } = req.body;
 
-    const newTodo = await prisma.todoList.create({
-      data: { name, status: 'TODO' },
-    })
+		const newTodo = await prisma.todoList.create({
+			data: { name, status: 'TODO' },
+		});
 
-    res.json(newTodo)
-  } catch (error: any) {
-    console.log(error.message, req)
-    res.status(500).json({
-      message: "Internal Server Error",
-    })
-  }
-})
+		res.json(newTodo);
+	} catch (error: any) {
+		console.log(error.message, req);
+		res.status(500).json({
+			message: 'Internal Server Error',
+		});
+	}
+});
 
 router.delete('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
+	const { id } = req.params;
 
-  try {
-    const deletedTodo = await prisma.todoList.delete({
-      where: { id }
-    })
-    res.send(deletedTodo)
-  } catch(error: any) {
-    console.log(error.message, req)
-    res.status(500).json({
-      message: "Internal Server Error",
-    })
-  }
-})
+	try {
+		const deletedTodo = await prisma.todoList.delete({
+			where: { id },
+		});
+		res.send(deletedTodo);
+	} catch (error: any) {
+		console.log(error.message, req);
+		res.status(500).json({
+			message: 'Internal Server Error',
+		});
+	}
+});
 
-module.exports = router
+module.exports = router;
