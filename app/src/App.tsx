c987afaeebac15f7b01972dc937cf0d9fc33e1dc
Joseph Diza <sephdiza@gmail.com>
2024-01-19 01:34:41 +0800

    add styles to clerk auth buttons

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,10 +10,14 @@ function App() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <div className="h-10 px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+          <SignInButton />
+        </div>
       </SignedOut>
       <SignedIn>
-        <SignOutButton />
+        <div className="h-10 px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+          <SignOutButton />
+        </div>
         <Todo />
       </SignedIn>
     </div>
